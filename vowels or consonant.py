ab8b7336240c0dbd25c04f1a906c0dfe481d1227
Rajesh3734 <51224096+Rajesh3734@users.noreply.github.com>
2019-07-12 17:25:51 +0530

    Update vowels or consonant.py

--- vowels or consonant.py
+++ vowels or consonant.py
@@ -2,7 +2,7 @@ a=input()
 b=["a","e","i","o","u","A","E","I","O","U"]
 if ((ord(a) in range(65,91))or(ord(a) in range(97,124))):
   if(a in b):
-    print("vowel")
+    print("Vowel")
   else:
     print("Consonant")
 else:
