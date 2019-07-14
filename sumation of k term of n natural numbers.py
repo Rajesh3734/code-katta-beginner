aa13e187a351225861cf4b9bb04ff0b37b687fbd
Rajesh3734 <51224096+Rajesh3734@users.noreply.github.com>
2019-07-14 22:59:55 +0530

    Update sumation of k term of n natural numbers.py

--- sumation of k term of n natural numbers.py
+++ sumation of k term of n natural numbers.py
@@ -3,9 +3,7 @@ sumation=0
 (n,k)=map(int,input().split())
 if((k<=n)and(k>=1)):
     for i in range(0,n):
-        n1.append(i+1)
-        print(n1[i])
+        n1.append(i+1)       
 for j in range(0,k):
-    
     sumation=sumation+n1[j]
 print(sumation)
