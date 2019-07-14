6a3f515f7aa65ecf7481417b2f572be2f4c28a65
Rajesh3734 <51224096+Rajesh3734@users.noreply.github.com>
2019-07-14 22:54:40 +0530

    Create sumation of k term of n natural numbers.py

---
+++ sumation of k term of n natural numbers.py
@@ -0,0 +1,11 @@
+n1=[]
+sumation=0
+(n,k)=map(int,input().split())
+if((k<=n)and(k>=1)):
+    for i in range(0,n):
+        n1.append(i+1)
+        print(n1[i])
+for j in range(0,k):
+    
+    sumation=sumation+n1[j]
+print(sumation)
