9434d917acdc61c211f079ad3b0a051a1a4f1284
Rajesh3734 <51224096+Rajesh3734@users.noreply.github.com>
2019-07-17 23:46:18 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,8 +1,17 @@
 (n,a,d)=map(int,input().split())
 sum=0
-for i in range(1,n+1):
-  if (i==1):
-    sum=a
-  sum=sum+d
-print(sum)
+l=[]
+sumation_of_n=0
+if((n>=1)and(d<=100000)):
+    for i in range(1,n+1):
+        if (i==1):
+            sum=a
+            l.append(sum)
+        else:
+            sum=sum+d
+            l.append(sum)
+print(l)
+for j in l:
+    sumation_of_n=sumation_of_n+j
+print(sumation_of_n)
 
